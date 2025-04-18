f29cfde5a83f8075483f0f2075bc8d0cdd4ce2e5
technicalxakash <akashr2468@gmail.com>
2025-04-18 23:20:59 +0530

    new

---
+++ app.py
@@ -0,0 +1,52 @@
+print("hello")
+
+for i in range(0,5):
+    print(i)
+
+
+
+for i in range(0,5):
+    for j in range(0,5):
+        print("* ",end=" ")
+    print(" ")
+
+
+
+for i in range(0,7):
+    for j in range(i):
+        print("* ",end=" ")
+    print(" ")
+
+
+
+for i in range(0,5):
+    for j in range(0,5):
+        print(i,end=" ")
+    print(" ")
+
+for i in range(0,7):
+    for j in range(i):
+        print(i,end=" ")
+    print(" ")
+
+
+
+for i in range(0,7):
+    for j in range(i):
+        print(j,end=" ")
+    print(" ")
+
+
+
+k=1;
+for i in range(0,6):
+   
+    for j in range(0,i+1):
+        print(k,end=" ")
+        k=k+1;
+    print(" ")
+
+
+
+
+    
